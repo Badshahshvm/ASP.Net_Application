2d6f82399c826bf3800b87c1c43e018f40f62d9a
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-08-03 09:57:43 +0530

    Create ProductModel.cs

---
+++ Models/ProductModel.cs
@@ -0,0 +1,11 @@
+namespace WebApplication_ForModel_To_Pass_Data_From_View_Controller.Models
+{
+    public class ProductModel
+    {
+        public int Price {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public string Category {  get; set; }
+    }
+}
